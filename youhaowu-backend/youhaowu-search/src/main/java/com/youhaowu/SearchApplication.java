2c799a2dce6a8bbf3a6f6691b876353cf72104d0
faris <faris.ni@outlook.com>
2026-06-01 13:37:50 +0800

    fix(search): 排除 DataSource 自动配置

--- youhaowu-backend/youhaowu-search/src/main/java/com/youhaowu/SearchApplication.java
+++ youhaowu-backend/youhaowu-search/src/main/java/com/youhaowu/SearchApplication.java
@@ -2,8 +2,9 @@ package com.youhaowu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class SearchApplication {
     public static void main(String[] args) {
         SpringApplication.run(SearchApplication.class, args);
